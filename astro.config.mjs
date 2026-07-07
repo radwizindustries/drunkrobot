0c16ce1ee3d8bd4a6935d920bb428660026ba1da
radwizindustries <radwizinc@gmail.com>
2026-07-07 09:36:45 -0700

    feat: archive grid, about page, legacy URL redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
+import { readFileSync } from 'node:fs';
 import { defineConfig } from 'astro/config';
+
+const comics = JSON.parse(readFileSync('./src/data/comics.json', 'utf8'));
+const redirects = {};
+for (const c of comics) {
+  redirects[`/${c.slug}`] = `/comic/${c.slug}/`;
+  const [y, m, d] = c.date.split('-');
+  redirects[`/${y}/${m}/${d}/${c.slug}`] = `/comic/${c.slug}/`;
+}
 
 export default defineConfig({
   site: 'https://drunk-robot.com',
+  redirects,
 });
